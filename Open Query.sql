0656e286c2849a29c3ae8a26ac68191de2d358c6
ncgraham77 <52003874+ncgraham77@users.noreply.github.com>
2019-06-19 10:51:16 -0400

    Add files via upload

---
+++ Open Query.sql
@@ -0,0 +1,41 @@
+--Select 
+--*
+
+
+--FROM
+--(
+SELECT TOP 100 *
+FROM
+	OPENQUERY (PSFT_FIN_PROD,'SELECT *
+								FROM 
+									PS_VCHR_LN_PO_VW
+										where
+											po_id = ''445379''
+											and BUSINESS_UNIT = ''12018''
+										
+  									
+	
+											
+
+											
+				') 
+
+--) A
+
+-- group by 
+-- a.PO_ID
+--,a.Business_unit
+--,a.Line_nbr
+
+
+
+
+
+--PS_PO_MVR_ACT_WRK_TOT_AMT_MATCHED
+--PS_PO_MTCH_LN_VW
+
+
+
+
+											--po_id = ''0000000289''
+											--and BUSINESS_UNIT = ''73000''
